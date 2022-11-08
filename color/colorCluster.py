11dadf599cb1300d09768fbf1b55c7f33bc6712d
hanu607 <hanu607@gmail.com>
2022-11-08 21:02:37 +0900

    K-Means Color

--- color/colorCluster.py
+++ color/colorCluster.py
@@ -1,5 +1,4 @@
 # reference : https://engineer-mole.tistory.com/250
-
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -17,18 +16,18 @@ def load_shoe_data(filename):
       shoes.data.append(items)
     shoes.data = np.array(shoes.data)
     return shoes
-shoes = load_shoe_data('data/shoes.csv')
 
+shoes = load_shoe_data('data/shoes.csv')
 fig = plt.figure(figsize = (8, 8))
 ax = fig.add_subplot(111, projection='3d')
 ax.set_title("Main Colors of Shoes (view_init(30, 330))", size = 20)
-ax.set_xlabel("R", size = 14)
-ax.set_ylabel("G", size = 14)
-ax.set_zlabel("B", size = 14)
-for colors in shoes.target:
-  RGBP = colors[0]
-  RGB = np.array(RGBP[:-1])/255.0
-  ax.scatter(RGB[0], RGB[1], RGB[2], color = RGB)
-ax.view_init(30, 330)
-plt.show()
-# 곡선을 그림
+# ax.set_xlabel("R", size = 14)
+# ax.set_ylabel("G", size = 14)
+# ax.set_zlabel("B", size = 14)
+# for colors in shoes.target:
+#   RGBP = colors[0]
+#   RGB = np.array(RGBP[:-1])/255.0
+#   ax.scatter(RGB[0], RGB[1], RGB[2], color = RGB)
+# ax.view_init(30, 330)
+# plt.show()
+
